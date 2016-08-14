eb5630a82f10a664a8c8f7fe6bcafac3df2efb53
Ivan Nikolić <niksy5@gmail.com>
2016-08-14 12:11:36 +0200

    Update definition for JS object properties

--- samples/test.js
+++ samples/test.js
@@ -66,8 +66,8 @@ var obj = {
  not legal.`
 
 // String interpolation
-var name = "Bob", time = "today";
-`Hello ${name}, how are you ${time}?`
+var name = "Bob", time = "today", foo = { bar: 1 };
+`Hello ${name}, how are you ${time}, ${foo.bar}?`
 
 // Construct an HTTP request prefix is used to interpret the replacements and construction
 POST`http://foo.org/bar?a=${a}&b=${b}`(myOnReadyStateChangeHandler);
@@ -313,41 +313,42 @@ sample javascript from xui
 
 var foo = 1;
 var bar = function(){
-	return bar;
+  return bar;
 };
 
 function foo(q,context){
-	return true;
+  return true;
 };
 
 var foo = {
-	bar: function () {
-		return 1;
-	},
-	baz: 1,
+  bar: function () {
+    return 1;
+  },
+  baz: 1,
 };
 
 debugger;
 
 o.foo();
 o.bar = 1;
+o.bar.baz = 1;
 
 var undefined,
-	xui,
-	window     = this,
-	string     = new String('string'),
-	document   = window.document,
-	simpleExpr = /^#?([\w-]+)$/,
-	idExpr     = /^#/,
-	tagExpr    = /<([\w:]+)/,
-	slice      = function (e) { return [].slice.call(e, 0); };
-	try { var a = slice(document.documentElement.childNodes)[0].nodeType; }
-	catch(e){ slice = function (e) { var ret=[]; for (var i=0; e[i]; i++)
-		ret.push(e[i]); return ret; }; }
+  xui,
+  window     = this,
+  string     = new String('string'),
+  document   = window.document,
+  simpleExpr = /^#?([\w-]+)$/,
+  idExpr     = /^#/,
+  tagExpr    = /<([\w:]+)/,
+  slice      = function (e) { return [].slice.call(e, 0); };
+  try { var a = slice(document.documentElement.childNodes)[0].nodeType; }
+  catch(e){ slice = function (e) { var ret=[]; for (var i=0; e[i]; i++)
+    ret.push(e[i]); return ret; }; }
 
 
 window.x$ = window.xui = xui = function(q, context) {
-	return new xui.fn.find(q, context);
+  return new xui.fn.find(q, context);
 };
 
 console.log( 'string', params );
